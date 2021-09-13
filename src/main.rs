ab93bf5938cb2e22b71ddaaaecff62d2d396a362
N0D4N <50947930+N0D4N@users.noreply.github.com>
2021-09-13 22:24:22 +0300

    Use rustfmt (#19)
    * Add rustfmt.toml
    
    * Format src
    
    * Edit ci

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,22 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #![deny(
-    clippy::all,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::cargo,
-    non_ascii_idents,
-    unsafe_code,
-    unused_crate_dependencies,
-    unused_extern_crates,
-    unused_import_braces,
-    unused_lifetimes,
-    unused_results
+	clippy::all,
+	clippy::pedantic,
+	clippy::nursery,
+	clippy::cargo,
+	non_ascii_idents,
+	unsafe_code,
+	unused_crate_dependencies,
+	unused_extern_crates,
+	unused_import_braces,
+	unused_lifetimes,
+	unused_results
 )]
 #![allow(
-    clippy::multiple_crate_versions,
-    clippy::semicolon_if_nothing_returned,
-    clippy::cargo_common_metadata
+	clippy::multiple_crate_versions,
+	clippy::semicolon_if_nothing_returned,
+	clippy::cargo_common_metadata
 )]
 
 use rand::{seq::SliceRandom, thread_rng};
@@ -46,111 +46,111 @@ const DATA_FILE_NAME: &str = "data.json";
 
 #[derive(Serialize, Deserialize)]
 struct Avatars {
-    /// Vec of pathes to avatars
-    avatars: Vec<String>,
-    /// Path to avatar currently being in use
-    current: String,
+	/// Vec of pathes to avatars
+	avatars: Vec<String>,
+	/// Path to avatar currently being in use
+	current: String,
 }
 
 #[derive(Deserialize)]
 struct Config {
-    /// Token of your Discord bot
-    token: String,
-    /// Path to directory with avatars in it
-    avatars_dir: String,
+	/// Token of your Discord bot
+	token: String,
+	/// Path to directory with avatars in it
+	avatars_dir: String,
 }
 
 #[tokio::main]
 async fn main() {
-    let config = get_config();
-    let mut avatars = get_current_state(&config);
+	let config = get_config();
+	let mut avatars = get_current_state(&config);
 
-    avatars.current = avatars.avatars.pop().unwrap();
-    save_current_state(&avatars);
+	avatars.current = avatars.avatars.pop().unwrap();
+	save_current_state(&avatars);
 
-    println!("New avatar will be {}", avatars.current);
-    change_avatar(&config.token, &avatars.current).await;
+	println!("New avatar will be {}", avatars.current);
+	change_avatar(&config.token, &avatars.current).await;
 }
 
 #[inline]
 fn save_current_state(avatars: &Avatars) {
-    write_to_file(
-        DATA_FILE_NAME,
-        json_to_string(avatars)
-            .unwrap_or_else(|_| panic!("Couldn't convert {:?} into proper json", avatars.avatars)),
-    )
-    .unwrap_or_else(|_| panic!("Couldn't write data file to {}", DATA_FILE_NAME));
+	write_to_file(
+		DATA_FILE_NAME,
+		json_to_string(avatars)
+			.unwrap_or_else(|_| panic!("Couldn't convert {:?} into proper json", avatars.avatars)),
+	)
+	.unwrap_or_else(|_| panic!("Couldn't write data file to {}", DATA_FILE_NAME));
 }
 
 #[inline]
 fn get_current_state(config: &Config) -> Avatars {
-    if Path::new(DATA_FILE_NAME).exists() {
-        let avatars: Avatars = json_from_file(DATA_FILE_NAME);
-        if avatars.avatars.is_empty() {
-            get_avatars(&config.avatars_dir, avatars.current)
-        } else {
-            avatars
-        }
-    } else {
-        get_avatars(&config.avatars_dir, String::from(""))
-    }
+	if Path::new(DATA_FILE_NAME).exists() {
+		let avatars: Avatars = json_from_file(DATA_FILE_NAME);
+		if avatars.avatars.is_empty() {
+			get_avatars(&config.avatars_dir, avatars.current)
+		} else {
+			avatars
+		}
+	} else {
+		get_avatars(&config.avatars_dir, String::from(""))
+	}
 }
 
 #[inline]
 fn get_config() -> Config {
-    json_from_file(CONFIG_FILE_NAME)
+	json_from_file(CONFIG_FILE_NAME)
 }
 
 async fn change_avatar(token: &str, path_to_new_avatar: &str) {
-    let http = HttpBuilder::new(&token)
-        .await
-        .expect("Couldn't' build http");
-    let base64 = read_image(&path_to_new_avatar).expect("Couldn't get image");
-    let mut current_user = http
-        .get_current_user()
-        .await
-        .expect("Couldn't get current user");
-    current_user
-        .edit(http, |p| p.avatar(Some(&base64)))
-        .await
-        .expect("Couldn't update profile picture");
+	let http = HttpBuilder::new(&token)
+		.await
+		.expect("Couldn't' build http");
+	let base64 = read_image(&path_to_new_avatar).expect("Couldn't get image");
+	let mut current_user = http
+		.get_current_user()
+		.await
+		.expect("Couldn't get current user");
+	current_user
+		.edit(http, |p| p.avatar(Some(&base64)))
+		.await
+		.expect("Couldn't update profile picture");
 }
 
 fn get_avatars(path: &str, current: String) -> Avatars {
-    let mut avatars: Vec<String> = read_dir(path)
-        .unwrap_or_else(|_| panic!("Couldn't read files from {} directory", path))
-        .filter(|x| x.as_ref().unwrap().file_type().unwrap().is_file())
-        .map(|x| x.as_ref().unwrap().path())
-        .filter(|x| {
-            matches!(
-                x.extension().unwrap_or_default().to_str().unwrap(),
-                "jpg" | "png"
-            )
-        })
-        .map(|y| String::from(y.to_str().unwrap()))
-        .collect();
-    if avatars.len() < 2 {
-        panic!(
-            "There must be 2 or more jpg/png files in {} directory to make use of discac utility",
-            path
-        );
-    }
-    let mut rng = thread_rng();
-    loop {
-        avatars.shuffle(&mut rng);
-        if !avatars.first().unwrap().eq(&current) {
-            break;
-        }
-    }
-    Avatars { avatars, current }
+	let mut avatars: Vec<String> = read_dir(path)
+		.unwrap_or_else(|_| panic!("Couldn't read files from {} directory", path))
+		.filter(|x| x.as_ref().unwrap().file_type().unwrap().is_file())
+		.map(|x| x.as_ref().unwrap().path())
+		.filter(|x| {
+			matches!(
+				x.extension().unwrap_or_default().to_str().unwrap(),
+				"jpg" | "png"
+			)
+		})
+		.map(|y| String::from(y.to_str().unwrap()))
+		.collect();
+	if avatars.len() < 2 {
+		panic!(
+			"There must be 2 or more jpg/png files in {} directory to make use of discac utility",
+			path
+		);
+	}
+	let mut rng = thread_rng();
+	loop {
+		avatars.shuffle(&mut rng);
+		if !avatars.first().unwrap().eq(&current) {
+			break;
+		}
+	}
+	Avatars { avatars, current }
 }
 
 fn json_from_file<T>(path: &str) -> T
 where
-    T: serde::de::DeserializeOwned,
+	T: serde::de::DeserializeOwned,
 {
-    json_from_reader(BufReader::new(
-        File::open(path).unwrap_or_else(|_| panic!("Couldn't open {} file", path)),
-    ))
-    .unwrap_or_else(|_| panic!("Couldn't parse {} as json", path))
+	json_from_reader(BufReader::new(
+		File::open(path).unwrap_or_else(|_| panic!("Couldn't open {} file", path)),
+	))
+	.unwrap_or_else(|_| panic!("Couldn't parse {} as json", path))
 }
