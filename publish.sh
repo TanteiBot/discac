5a91987c74ee07483caf5aa42c43c607fab6e18c
N0D4N <50947930+N0D4N@users.noreply.github.com>
2021-12-23 15:14:39 +0200

    Make scripts executable, put them to scripts folder

--- publish.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-/usr/bin/env sh ./build.sh "$1" && mkdir -p "./output/" && cp "./target/$1/release/discac" "./output/discac" && cp temp.config.json "./output/temp.config.json"
---
+++ scripts/publish-x86-64-Linux.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+./publish.sh x86_64-unknown-linux-gnu
