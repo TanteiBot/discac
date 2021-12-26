7e870edd7f60084eef9eb6916cc45e7cc87c083d
N0D4N <50947930+N0D4N@users.noreply.github.com>
2021-12-26 19:45:19 +0200

    Add multi-profile support (#46)

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,16 @@ use rand::{seq::SliceRandom, thread_rng};
 use serde::{Deserialize, Serialize};
 use serde_json::{from_reader as json_from_reader, to_string_pretty as json_to_string};
 use serenity::{http::client::HttpBuilder, utils::read_image};
+use std::env;
+use std::ffi::OsStr;
 use std::fs::{canonicalize as to_absolute_path, read_dir, write as write_to_file, File};
 use std::io::BufReader;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 const CONFIG_FILE_NAME: &str = "config.json";
 const DATA_FILE_NAME: &str = "data.json";
+const FOLDER_WITH_PROFILES_ENV_VAR_NAME: &str = "DISCAC_PROFILES_DIR";
 
 #[derive(Serialize, Deserialize)]
 struct Avatars {
@@ -60,32 +64,85 @@ struct Config {
 	avatars_dir: String,
 }
 
+struct Pathes {
+	path_to_config: Box<Path>,
+	path_to_data: Box<Path>,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-	let config = get_config();
-	let mut avatars = get_current_state(&config);
+	let pathes = get_config_and_data_path();
+	let config = get_config(&pathes.path_to_config);
+	let mut avatars = get_current_state(&config, &pathes.path_to_data);
 
 	avatars.current = Option::Some(avatars.avatars.remove(0));
-	save_current_state(&avatars);
+	save_current_state(&avatars, &pathes.path_to_data);
 
 	println!("New avatar will be {}", avatars.current.as_ref().unwrap());
 	change_avatar(&config.token, &avatars.current.unwrap()).await;
 }
 
-#[inline]
-fn save_current_state(avatars: &Avatars) {
+fn save_current_state(avatars: &Avatars, path_to_data: &Path) {
 	write_to_file(
-		DATA_FILE_NAME,
+		path_to_data,
 		json_to_string(avatars)
 			.unwrap_or_else(|_| panic!("Couldn't convert {:?} into proper json", avatars.avatars)),
 	)
-	.unwrap_or_else(|_| panic!("Couldn't write data file to {}", DATA_FILE_NAME));
+	.unwrap_or_else(|_| panic!("Couldn't write data file to {:?}", path_to_data));
 }
 
-#[inline]
-fn get_current_state(config: &Config) -> Box<Avatars> {
-	if Path::new(DATA_FILE_NAME).exists() {
-		let mut avatars: Box<Avatars> = json_from_file(DATA_FILE_NAME);
+fn get_config_and_data_path() -> Box<Pathes> {
+	let dir_with_data_and_config = get_dir_with_data_and_config();
+	let mut vec = [
+		dir_with_data_and_config,
+		Box::<OsStr>::from(DATA_FILE_NAME.as_ref()),
+	];
+	let path_to_data = get_joined_path(&vec);
+	vec[1] = Box::<OsStr>::from(CONFIG_FILE_NAME.as_ref());
+	let path_to_config = get_joined_path(&vec);
+	assert!(
+		path_to_config.is_file(),
+		"{}",
+		format!("{:?} isn't a file", path_to_config)
+	);
+	Box::<Pathes>::new(Pathes {
+		path_to_config,
+		path_to_data,
+	})
+}
+
+fn get_dir_with_data_and_config() -> Box<OsStr> {
+	if let Ok(val) = env::var(FOLDER_WITH_PROFILES_ENV_VAR_NAME) {
+		let dir_with_profiles = Path::new(&val);
+		assert!(
+			dir_with_profiles.is_dir(),
+			"{}",
+			format!(
+				"Value of {} environment variable isn't a directory",
+				FOLDER_WITH_PROFILES_ENV_VAR_NAME
+			)
+		);
+		let profile_name = &env::args().nth(1).expect("Can't get name of profile");
+
+		let path_to_dir_with_data_and_config = Box::<OsStr>::from(
+			env::join_paths([dir_with_profiles.to_str().unwrap(), profile_name.as_str()]).unwrap(),
+		);
+		assert!(
+			Path::new(&path_to_dir_with_data_and_config).is_dir(),
+			"{}",
+			format!("{:?} isn't a directory", &path_to_dir_with_data_and_config)
+		);
+		path_to_dir_with_data_and_config
+	} else {
+		println!("Environment variable \"{0}\" not set, assuming single profile mode, where \"{1}\" and \"{2}\" are located in the same directory as \"discac\" executable", FOLDER_WITH_PROFILES_ENV_VAR_NAME, DATA_FILE_NAME, CONFIG_FILE_NAME);
+		let current_dir = env::current_dir().expect("Couldn't get current dir");
+		Box::<OsStr>::from(current_dir.as_os_str())
+	}
+}
+
+fn get_current_state(config: &Config, path_to_data: &Path) -> Box<Avatars> {
+	if path_to_data.exists() {
+		let mut avatars: Box<Avatars> = json_from_file(path_to_data);
 		if avatars.avatars.is_empty() {
 			avatars.avatars = get_avatars(&config.avatars_dir);
 			let mut rng = thread_rng();
@@ -100,6 +157,7 @@ fn get_current_state(config: &Config) -> Box<Avatars> {
 		}
 		avatars
 	} else {
+		println!("File with data doesn't exist");
 		Box::<Avatars>::new(Avatars {
 			avatars: get_avatars(&config.avatars_dir),
 			current: Option::None,
@@ -107,9 +165,8 @@ fn get_current_state(config: &Config) -> Box<Avatars> {
 	}
 }
 
-#[inline]
-fn get_config() -> Box<Config> {
-	json_from_file(CONFIG_FILE_NAME)
+fn get_config(path_to_config: &Path) -> Box<Config> {
+	json_from_file(path_to_config)
 }
 
 async fn change_avatar(token: &str, path_to_new_avatar: &str) {
@@ -161,12 +218,24 @@ fn get_avatars(path: &str) -> Vec<String> {
 	avatars
 }
 
-fn json_from_file<T>(path: &str) -> T
+fn json_from_file<T>(path: &Path) -> T
 where
 	T: serde::de::DeserializeOwned,
 {
 	json_from_reader(BufReader::new(
-		File::open(path).unwrap_or_else(|_| panic!("Couldn't open {} file", path)),
+		File::open(path).unwrap_or_else(|_| panic!("Couldn't open {:?} file", path)),
 	))
-	.unwrap_or_else(|_| panic!("Couldn't parse {} as json", path))
+	.unwrap_or_else(|_| panic!("Couldn't parse {:?} as json", path))
+}
+
+fn get_joined_path(arr: &[Box<OsStr>; 2]) -> Box<Path> {
+	Box::<Path>::from(
+		PathBuf::from_str(
+			env::join_paths(arr)
+				.expect("Couldn't join pathes")
+				.to_str()
+				.expect("Couldn't transform os-specific string to str"),
+		)
+		.unwrap(),
+	)
 }
