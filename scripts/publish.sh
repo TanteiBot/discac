45636c3330521c9c3cae3dbe3471735a3d4eeaea
N0D4N <50947930+N0D4N@users.noreply.github.com>
2022-01-13 13:45:41 +0200

    Put systemd and license files in publish directory

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-./scripts/build.sh "$1" && mkdir -p "./output/" && cp "./target/$1/release/discac" "./output/discac" && cp temp.config.json "./output/temp.config.json"
+./scripts/build.sh "$1" && mkdir -p "./output/" && cp "./target/$1/release/discac" "./output/discac" && cp temp.config.json "./output/temp.config.json" && cp "./systemd/" "./output/" && cp LICENSE "./output/"
